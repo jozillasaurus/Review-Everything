230efb6691b86b2c9f9cd12e9de86e6acfb969da
jozillasaurus <jozilla3@gmail.com>
2020-12-03 13:43:38 -0800

    rails gems

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class PostsController < ApplicationController
+      # protect_from_forgery with: :null_session
+
       def index
         posts = Post.all
 
--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,9 +1,10 @@
 module Api
   module V1
     class ReviewsController < ApplicationController
-
+      protect_from_forgery with: :null_session
+      
       def create
-        review = Review.new(review_params)
+        review = post.reviews.new(review_params)
 
         if review.save
           render json: ReviewSerializer.new(review).serializable_hash
@@ -23,6 +24,10 @@ module Api
       end
 
       private
+
+      def post
+        @post ||= Post.find(params[:post_id])
+      end
 
       def review_params
         params.require(:review).permit(:title, :description, :score, :post_id)
